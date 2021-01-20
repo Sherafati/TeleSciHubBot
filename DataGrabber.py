edb3c71590571acecd7a15943b43e7da5e98ce81
Arian Sherafati <69812147+Sherafati@users.noreply.github.com>
2021-01-20 19:12:41 +0330

    Update DataGrabber.py

--- DataGrabber.py
+++ DataGrabber.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-payload = {"reuqest":28866452}
-r = requests.post("https://sci-hub.do", data = payload)
+#payload = {"reuqest":28866452}
+r = requests.get("https://sci-hub.do")
 print(r.text)
 
 # class botHandler:
