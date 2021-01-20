f7b1807c333ae2f3b77f87d942db148a6b3f08c2
Arian Sherafati <69812147+Sherafati@users.noreply.github.com>
2021-01-20 22:58:17 +0330

    Update DataGrabber.py

--- DataGrabber.py
+++ DataGrabber.py
@@ -6,8 +6,9 @@ import re
 headers = {"user-agent":"Mozilla/5.0"}
 url = "https://sci-hub.do"
 payload = {"request": "28866452"}
-r = requests.post(url, data = payload, headers = headers)
-print(r.status_code)
+with requests.Session() as s:
+        r = s.post(url, data = payload, headers = headers)
+        print(r.status_code)
 
 
 # class botHandler:
