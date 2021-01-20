b2c45b1a5b97b1662bd498cf16319451cd3a69cc
Arian Sherafati <69812147+Sherafati@users.noreply.github.com>
2021-01-20 19:49:29 +0330

    Update DataGrabber.py

--- DataGrabber.py
+++ DataGrabber.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 payload = {"reuqest":28866452}
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0", "Referer":"https://sci-hub.do/"}
 r = requests.get("https://sci-hub.do")
 print(r.text)
 
